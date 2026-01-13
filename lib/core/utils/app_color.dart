04a22c25d923140dbce13fe7b9a35db7d6b78214
aymanshawqi360 <aymanshawqi380@gmail.com>
2026-01-13 10:14:52 +0300

    -added app_color to folder utils

---
+++ lib/core/utils/app_color.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static Color primaryColor = const Color(0xff452CE8);
+  static Color secondary = const Color(0xffFBFBFC);
+  static Color background = const Color(0xffF4F4F4);
+
+  // ===== Light Mode =====
+
+  // ======= griy Colors ======
+  static Color griy50 = Color(0xffFBFBFC);
+  static Color griy100 = Color(0xffeeeef1);
+  static Color griy200 = Color(0xffe0e0e5);
+  static Color griy300 = Color(0xffcecdd4);
+  static Color griy400 = Color(0xffc2c1c9);
+  static Color griy500 = Color(0xffa7a5af);
+  static Color griy600 = Color(0xff938f9c);
+  static Color griy700 = Color(0xff7f7b87);
+  static Color griy800 = Color(0xff68656e);
+  static Color griy900 = Color(0xff57545b);
+
+  // ====== Alert Colors ======
+
+  //Error
+  static Color alertError400 = Color(0xffff6979);
+  static Color alertError500 = Color(0xfffe354e);
+  static Color alertError600 = Color(0xffde1135);
+
+  //Success
+  static Color alertSucces500 = Color(0xff18cf6d);
+  static Color alertSucces600 = Color(0xff0dac57);
+  static Color alertSucces700 = Color(0xff0e8345);
+
+  // ====== Common Colors ======
+  static Color black = Color(0xff000000);
+  static Color white = Color(0xffffffff);
+  static Color transparent = Colors.transparent;
+
+  // ====== Shadow Colors ======
+  static Color shadowColor = Color(0xff000000);
+
+  // ====== Light Text ======
+  static Color textBlack = Color(0xff323135);
+  static Color textGray = Color(0xff68656E);
+  static Color textWhite = Color(0xffFBFBFC);
+  // ====== Dark Text ======
+  // static Color textWhite = Color(0xffFBFBFC);
+}
