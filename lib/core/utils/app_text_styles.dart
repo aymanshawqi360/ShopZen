390fc3e09b3ca634d86010e3cdc74c12963b2590
aymanshawqi360 <aymanshawqi380@gmail.com>
2026-01-13 10:15:31 +0300

    -added app_text_styles to folder utils

---
+++ lib/core/utils/app_text_styles.dart
@@ -0,0 +1,342 @@
+import 'package:flutter/material.dart';
+import 'package:shopzen/core/constants/app_strings.dart';
+import 'package:shopzen/core/utils/font_weight_helper.dart';
+
+class AppTextStyles {
+  // Display
+  static const TextStyle displayLarge = TextStyle(
+    fontFamily: AppStrings.satoshiFont,
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle displayMedium = TextStyle(
+    fontFamily: AppStrings.satoshiFont,
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+  );
+
+   // Headlines
+  static const TextStyle headlineLarge = TextStyle(
+    fontFamily: AppStrings.satoshiFont,
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle headlineMedium = TextStyle(
+    fontFamily: AppStrings.satoshiFont,
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle headlineSmall = TextStyle(
+    fontFamily: AppStrings.satoshiFont,
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+  // Onboarding
+  static const TextStyle onboardingLarge = TextStyle(
+    fontFamily: AppStrings.satoshiFont,
+    fontSize: 32,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static const TextStyle onboardingBody = TextStyle(
+    fontFamily: AppStrings.satoshiFont,
+    fontSize: 18,
+    fontWeight: FontWeightHelper.medium,
+  );
+  static const TextStyle onboardingSmall = TextStyle(
+    fontFamily: AppStrings.satoshiFont,
+    fontSize: 16,
+    fontWeight: FontWeightHelper.light,
+  );
+  // Titles
+  static const TextStyle titleLarge = TextStyle(
+   // fontFamily: AppStrings.appFontNameLato,
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle titleMedium = TextStyle(
+  //  fontFamily: AppStrings.appFontNameLato,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle titleSmall = TextStyle(
+  //  fontFamily: AppStrings.appFontNameLato,
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+  );
+  // Body
+  static const TextStyle bodyLarge = TextStyle(
+   // fontFamily: AppStrings.appFontNameLato,
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle bodyMedium = TextStyle(
+  //  fontFamily: AppStrings.appFontNameLato,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle bodySmall = TextStyle(
+  //  fontFamily: AppStrings.appFontNameLato,
+    fontSize: 12,
+    fontWeight: FontWeight.w300,
+  );
+
+  // Titles
+  //   static const TextStyle titleLarge = TextStyle(
+  //     fontFamily: AppStrings.appFontNameLato,
+  //     fontSize: 16,
+  //     fontWeight: FontWeight.w700,
+  //   );
+
+  //   static const TextStyle titleMedium = TextStyle(
+  //     fontFamily: AppStrings.appFontNameLato,
+  //     fontSize: 16,
+  //     fontWeight: FontWeight.w500,
+  //   );
+
+  //   static const TextStyle titleSmall = TextStyle(
+  //     fontFamily: AppStrings.appFontNameLato,
+  //     fontSize: 14,
+  //     fontWeight: FontWeight.w600,
+  //   );
+
+  //   // Body
+  //   static const TextStyle bodyLarge = TextStyle(
+  //     fontFamily: AppStrings.appFontNameLato,
+  //     fontSize: 16,
+  //     fontWeight: FontWeight.w400,
+  //   );
+
+  //   static const TextStyle bodyMedium = TextStyle(
+  //     fontFamily: AppStrings.appFontNameLato,
+  //     fontSize: 14,
+  //     fontWeight: FontWeight.w400,
+  //   );
+
+  //   static const TextStyle bodySmall = TextStyle(
+  //     fontFamily: AppStrings.appFontNameLato,
+  //     fontSize: 12,
+  //     fontWeight: FontWeight.w300,
+  //   );
+
+  //   // Labels
+  //   static const TextStyle labelLarge = TextStyle(
+  //     // fontFamily: AppStrings.appFontNameLato,
+  //     fontSize: 14,
+  //     fontWeight: FontWeight.w700,
+  //   );
+
+  //   static const TextStyle labelMedium = TextStyle(
+  //     fontFamily: AppStrings.fontFamily,
+  //     fontSize: 12,
+  //     fontWeight: FontWeight.w600,
+  //   );
+
+  //   static const TextStyle labelSmall = TextStyle(
+  //     // fontFamily: AppStrings.appFontNameLato,
+  //     fontSize: 10,
+  //     fontWeight: FontWeight.w300,
+  //   );
+
+  //   // Price
+  //   static const TextStyle priceUp = TextStyle(
+  //     fontFamily: AppStrings.appFontNameLato,
+  //     fontSize: 14,
+  //     fontWeight: FontWeight.w700,
+  //     color: Color(0xFF00CB6A),
+  //   );
+
+  //   static const TextStyle priceDown = TextStyle(
+  //     fontFamily: AppStrings.appFontNameLato,
+  //     fontSize: 14,
+  //     fontWeight: FontWeight.w700,
+  //     color: Color(0xFFF26666),
+  //   );
+
+  //   // Auth
+  //   static const TextStyle authTitle = TextStyle(
+  //     // fontFamily: 'Lato',
+  //     fontSize: 28,
+  //     fontWeight: FontWeight.w700,
+  //   );
+
+  //   static const TextStyle authSubtitle = TextStyle(
+  //     // fontFamily: 'Lato',
+  //     fontSize: 16,
+  //     fontWeight: FontWeight.w400,
+  //   );
+
+  //   static const TextStyle authButton = TextStyle(
+  //       //fontFamily: 'Lato',
+  //     fontSize: 18,
+  //     fontWeight: FontWeight.w700,
+  //   );
+
+  //   static const TextStyle authFooter = TextStyle(
+  //     //fontFamily: 'Lato',
+  //     fontSize: 14,
+  //     fontWeight: FontWeight.w400,
+  //   );
+
+  //   static const TextStyle authFooterLink = TextStyle(
+  //    // fontFamily: 'Lato',
+  //     fontSize: 14,
+  //     fontWeight: FontWeight.w600,
+  //   );
+
+  //   static const TextStyle authFooterQuestion = TextStyle(
+  //   //  fontFamily: 'Lato',
+  //     fontSize: 18,
+  //     fontWeight: FontWeight.w500,
+  //     height: 1.47,
+  //   );
+
+  //   static const TextStyle authFooterAction = TextStyle(
+  //    // fontFamily: 'Lato',
+  //     fontSize: 18,
+  //     fontWeight: FontWeight.w500,
+  //     height: 1.47,
+  //   );
+
+  //   static const TextStyle authSuccessTitle = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 22,
+  //     fontWeight: FontWeight.w700,
+  //   );
+
+  //   static const TextStyle authSuccessSubtitle = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 16,
+  //     fontWeight: FontWeight.w400,
+  //     height: 1.5,
+  //   );
+
+  //   static const TextStyle authBiometricTitle = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 26,
+  //     fontWeight: FontWeight.w700,
+  //   );
+
+  //   static const TextStyle authBiometricDescription = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 18,
+  //     fontWeight: FontWeight.w600,
+  //     height: 1.33,
+  //   );
+
+  //   static const TextStyle authBiometricLabel = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 18,
+  //     fontWeight: FontWeight.w600,
+  //   );
+
+  //   static const TextStyle authBiometricScanInstruction = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 18,
+  //     fontWeight: FontWeight.w500,
+  //     height: 1.40,
+  //   );
+
+  //   static const TextStyle authBiometricIconLabel = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 20,
+  //     fontWeight: FontWeight.w600,
+  //   );
+
+  //   static const TextStyle authBiometricSuccessTitle = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 26,
+  //     fontWeight: FontWeight.w700,
+  //   );
+
+  //   static const TextStyle authBiometricIllustrationDescription = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 16,
+  //     fontWeight: FontWeight.w600,
+  //     height: 1.5,
+  //   );
+
+  //   static const TextStyle authSuccessSectionTitle = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 22,
+  //     fontWeight: FontWeight.w600,
+  //   );
+
+  //   static const TextStyle authSuccessSectionSubtitle = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 16,
+  //     fontWeight: FontWeight.w400,
+  //   );
+
+  //   static const TextStyle authHeaderTitle = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 26,
+  //     fontWeight: FontWeight.w700,
+  //   );
+
+  //   static const TextStyle authHeaderSubtitle = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 18,
+  //     fontWeight: FontWeight.w600,
+  //     height: 1.33,
+  //   );
+
+  //   static const TextStyle authTextFieldInput = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 14,
+  //     fontWeight: FontWeight.w400,
+  //   );
+
+  //   static const TextStyle authTextFieldHint = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 14,
+  //     fontWeight: FontWeight.w400,
+  //   );
+
+  // // rahma
+  //   static const TextStyle errorLoading = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 16,
+  //     fontWeight: FontWeight.w700,
+  //     // color: AppColors.error,
+  //   );
+
+  //   static const TextStyle baseSmall = TextStyle(
+  //       fontSize: 12,
+  //   );
+
+  //   static const TextStyle labelMoreLarge = TextStyle(
+  //       fontSize: 15,
+  //       fontWeight: FontWeight.w700,
+  //   );
+
+  //   static const TextStyle headlineSmallBold = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 18,
+  //     fontWeight: FontWeight.w700,
+  //   );
+
+  //   static const TextStyle titleMoreSmall = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 14,
+  //     fontWeight: FontWeight.w500,
+  //   );
+
+  //   static const TextStyle headlineMediumBold = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 20,
+  //     fontWeight: FontWeight.w700, // SemiBold
+  //   );
+
+  //   static const TextStyle titleLargesemiBold = TextStyle(
+  //     fontFamily: 'Lato',
+  //     fontSize: 16,
+  //     fontWeight: FontWeight.w600,
+  //   );
+}
