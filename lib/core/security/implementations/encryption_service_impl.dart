970690301902b73a2923d212634f39dca2195268
aymanshawqi360 <aymanshawqi380@gmail.com>
2026-01-24 10:17:28 +0300

    - added securty folder

---
+++ lib/core/security/implementations/encryption_service_impl.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+import 'dart:math';
+import 'dart:typed_data';
+
+import 'package:dartz/dartz.dart';
+import 'package:shopzen/core/config/env_config.dart';
+import 'package:shopzen/core/error/api_error_model.dart';
+import 'package:shopzen/core/error/stoarge_failure_message.dart';
+import 'package:shopzen/core/security/implementations/flutter_secure_storage_impl.dart';
+import 'package:shopzen/core/security/interfaces/i_encryption_service.dart';
+import 'package:encrypt/encrypt.dart' as enc;
+
+class EncryptionServiceImpl implements IEncryptionService {
+  final FlutterSecureStorageImpl flutterSecureStorage;
+  final EnvConfig envConfig;
+
+  const EncryptionServiceImpl({
+    required this.flutterSecureStorage,
+    required this.envConfig,
+  });
+
+  //Encrypt data
+  @override
+  Future<Either<Failure, String>> encrypt() async {
+    try {
+      final key = await _createAndSaveKey();
+      final getToken = await flutterSecureStorage.read(
+        key: envConfig.getEncryptionToken(),
+      );
+      String? token = getToken.fold((_) => null, (token) => token);
+      if (token == null || token.isEmpty) {
+        return Left(Failure(errorMessage: StoargeFailureMessage.noToken));
+      }
+
+      final iv = enc.IV.fromSecureRandom(16);
+      final encrypter = enc.Encrypter(
+        enc.AES(key, mode: enc.AESMode.cbc, padding: 'PKCS7'),
+      );
+      final encrypt = encrypter.encrypt(token, iv: iv);
+      final encode = base64Url.encode(iv.bytes + encrypt.bytes);
+      return Right(encode);
+    } catch (e) {
+      return Left(Failure(errorMessage: StoargeFailureMessage.failedToDecrypt));
+    }
+  }
+
+  //Decrypt data
+  @override
+  Future<Either<Failure, String>> decrypt({String? cipherText}) async {
+    try {
+      final key = await _createAndSaveKey();
+
+      if (cipherText == null || cipherText.isEmpty) {
+        return Left(Failure(errorMessage: StoargeFailureMessage.noToken));
+      }
+      Uint8List decode = base64Url.decode(cipherText);
+      final iv = enc.IV(decode.sublist(0, 16));
+      final cipher = enc.Encrypted(decode.sublist(16));
+      final encrypter = enc.Encrypter(
+        enc.AES(key, mode: enc.AESMode.cbc, padding: 'PKCS7'),
+      );
+      final decrypt = encrypter.decrypt(cipher, iv: iv);
+      return Right(decrypt);
+    } catch (e) {
+      return Left(Failure(errorMessage: StoargeFailureMessage.failedToDecrypt));
+    }
+  }
+
+  Future<enc.Key> _createAndSaveKey() async {
+    final key = await flutterSecureStorage.read(
+      key: envConfig.getEncryptionKey(),
+    );
+
+    String? keyValue = key.fold((_) => null, (key) => key);
+    if (keyValue != null && keyValue.isNotEmpty) {
+      final byte = base64Url.decode(keyValue);
+      return enc.Key(Uint8List.fromList(byte));
+    }
+    final random = Random.secure();
+    final keyBytes = List<int>.generate(32, (index) => random.nextInt(256));
+    final encode = base64UrlEncode(keyBytes);
+    await flutterSecureStorage.write(
+      key: envConfig.getEncryptionKey(),
+      value: encode,
+    );
+    return enc.Key(Uint8List.fromList(keyBytes));
+  }
+}
---
+++ lib/core/security/implementations/flutter_secure_storage_impl.dart
@@ -0,0 +1,102 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shopzen/core/error/stoarge_failure_message.dart';
+import 'package:shopzen/core/security/interfaces/i_secure_storage.dart';
+
+class FlutterSecureStorageImpl implements ISecureStorage {
+  final FlutterSecureStorage _flutterSecureStorage;
+
+  FlutterSecureStorageImpl({
+    required FlutterSecureStorage? flutterSecureStorage,
+  }) : _flutterSecureStorage =
+           flutterSecureStorage ??
+           const FlutterSecureStorage(
+             aOptions: AndroidOptions(encryptedSharedPreferences: true),
+             iOptions: IOSOptions(
+               accessibility: KeychainAccessibility.first_unlock,
+             ),
+             mOptions: MacOsOptions(
+               accessibility: KeychainAccessibility.first_unlock,
+             ),
+           );
+  @override
+  Future<Either<StorageErrorModel, bool>> containsKey({required String key}) async{
+   try {
+    bool isExist = await _flutterSecureStorage.containsKey(key: key);
+     return Right(isExist);
+   } catch (e) {
+     return Left( 
+       DataNotFoundFailure(message: StoargeFailureMessage.keyNotFound)
+     );
+     
+   }
+  }
+
+  @override
+  Future<Either<StorageErrorModel, void>> delete({required String key}) async{
+    try {
+      await _flutterSecureStorage.delete(key: key);
+      return Right(null);
+    } catch (e) {
+      return Left( 
+        StorageDeleteFailure()
+      );
+      
+    }
+  }
+
+  @override
+  Future<Either<StorageErrorModel, void>> deleteAll() async{
+    
+    try {
+        await _flutterSecureStorage.deleteAll();
+      return Right(null);
+    } catch (error) {
+
+      return Left(StorageDeleteAllFailure());
+      
+    }
+  }
+
+  @override
+  Future<Either<StorageErrorModel, String?>> read({required String key}) async {
+    try {
+      String? reas = await _flutterSecureStorage.read(key: key);
+      return Right(reas);
+    } catch (e) {
+      return Left( 
+        StorageReadFailure()
+      );
+       
+    }
+  }
+
+  @override
+  Future<Either<StorageErrorModel, Map<String, String>>> readAll() async{
+    try {
+      final reasAll=await _flutterSecureStorage.readAll();
+      return Right(reasAll);
+    } catch (e) {
+      return Left( 
+        StorageReadAllFailure(message:e.toString())
+      );
+      
+    }
+  }
+
+  @override
+  Future<Either<StorageErrorModel, void>> write({
+    required String key,
+    required String value,
+  }) async{
+    try {
+     await _flutterSecureStorage.write(key: key, value: value);
+      return Right(null);
+    } catch (e) {
+      return Left( 
+        StorageWriteFailure()
+      );
+      
+    }
+  }
+}
