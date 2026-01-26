773e09c271c7c629b74a2dcfc7af343916572518
aymanshawqi360 <aymanshawqi380@gmail.com>
2026-01-26 21:48:51 +0300

    -added an update to data sources this update is add login function

--- lib/feature/auth/data/data_sources/auth_remote_data_source.dart
+++ lib/feature/auth/data/data_sources/auth_remote_data_source.dart
@@ -1,8 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:shopzen/core/constants/api_constants.dart';
+import 'package:shopzen/feature/auth/data/model/login/login_request_model.dart';
 import 'package:shopzen/feature/auth/data/model/register/register_request_model.dart';
-import 'package:shopzen/feature/auth/data/model/register/register_response_model.dart';
+import 'package:shopzen/core/Shared/model/auth_response_model.dart';
 
 part 'auth_remote_data_source.g.dart';
 
@@ -11,7 +12,10 @@ abstract class AuthApiService {
   factory AuthApiService(Dio dio, {String? baseUrl}) = _AuthApiService;
 
   @POST(EndPonts.register)
-  Future<RegisterResponseModel> register({
+  Future<AuthResponseModel> register({
       @Body() required RegisterRequestModel body});
+
+  @POST(EndPonts.login)
+  Future<AuthResponseModel> login({@Body() required LoginRequestModel body});    
 }
 
