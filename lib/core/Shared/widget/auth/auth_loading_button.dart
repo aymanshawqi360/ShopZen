ef352afba8d8ea2cc729c1479f766bce358e0bc6
aymanshawqi360 <aymanshawqi380@gmail.com>
2026-01-27 22:43:28 +0300

    - added shard auth_loading_button file between register and login features

---
+++ lib/core/Shared/widget/auth/auth_loading_button.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shopzen/core/common_ui/widgets/custom_button.dart';
+import 'package:shopzen/core/constants/app_size.dart';
+import 'package:shopzen/core/extension/app_extension.dart';
+import 'package:shopzen/core/utils/app_color.dart';
+import 'package:shopzen/core/utils/font_weight_helper.dart';
+
+class AuthLoadingButton<C extends Cubit<S>, S> extends StatelessWidget {
+  final void Function() checkUserValue;
+  final bool Function(S state) isLoading;
+
+  const AuthLoadingButton({
+    super.key,
+    required this.checkUserValue,
+    required this.isLoading,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        AppSize.gapH30,
+        BlocSelector<C, S, bool>(
+          selector: (state) => isLoading(state),
+          builder: (context, state) {
+            return CustomButton(
+              // key: Key("register_button"),
+              child: state
+                  ? Transform.scale(
+                      scale: AppSize.progressIndicatorInLoadingState,
+                      child: CircularProgressIndicator(
+                        strokeWidth: AppSize.progressIndicatorStrokeWidth,
+                        backgroundColor:context.islight()? AppColors.lightBackground: AppColors.darkBackground,
+                      ),
+                    )
+                  : Text(
+                      context.l10n.register,
+                      style: context.appTheme.textTheme.bodyLarge?.copyWith(
+                        color:context.islight()? AppColors.textWhite:AppColors.textDark,
+                        fontWeight: FontWeightHelper.bold,
+                        fontSize: AppSize.textSizeSp13,
+                      ),
+                    ),
+
+              onPressed: () {
+                checkUserValue();
+              },
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
