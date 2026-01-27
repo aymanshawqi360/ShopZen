2cb6e9e0e10c7243849f3d58de3f75d47355dc7b
aymanshawqi360 <aymanshawqi380@gmail.com>
2026-01-27 22:45:27 +0300

    -added shared slide_and_fade_auth file between register and login features

---
+++ lib/core/Shared/widget/auth/slide_and_fade_auth.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class SlideAndFadeAuth extends StatefulWidget {
+  final Widget child;
+  const SlideAndFadeAuth({super.key, required this.child});
+
+  @override
+  State<SlideAndFadeAuth> createState() => _SlideAndFadeAuthState();
+}
+
+class _SlideAndFadeAuthState extends State<SlideAndFadeAuth>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: Offset(0.0, 0.3),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    _fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _slideAnimation,
+      child: FadeTransition(opacity: _fadeAnimation, child: widget.child),
+    );
+  }
+}
