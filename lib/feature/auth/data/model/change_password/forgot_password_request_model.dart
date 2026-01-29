5c6e8be62fab9aa452a0c98acc27c6af55dcaa00
aymanshawqi360 <aymanshawqi380@gmail.com>
2026-01-29 17:00:12 +0300

    -added forgot_password_request & forgot_password_response models

---
+++ lib/feature/auth/data/model/change_password/forgot_password_request_model.dart
@@ -0,0 +1,12 @@
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'forgot_password_request_model.g.dart';
+@JsonSerializable()
+class ForgotPasswordRequestModel {
+  String? email;
+  
+  ForgotPasswordRequestModel({this.email});
+  factory ForgotPasswordRequestModel.fromJson(Map<String, dynamic> json) => _$ForgotPasswordRequestModelFromJson(json);
+  Map<String, dynamic> toJson() => _$ForgotPasswordRequestModelToJson(this);
+}
