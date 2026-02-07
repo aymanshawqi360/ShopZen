6ad2817a0c2ca8d32ea2c6c8eb1fc074078b264c
aymanshawqi360 <aymanshawqi380@gmail.com>
2026-02-07 20:55:32 +0300

    - Refactor password update UI by extracting form widget

---
+++ lib/feature/auth/presentation/widget/password_update/password_update_form.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:shopzen/core/common_ui/widgets/custom_button.dart';
+import 'package:shopzen/core/common_ui/widgets/custom_text_field.dart';
+import 'package:shopzen/core/constants/app_size.dart';
+import 'package:shopzen/core/extension/app_extension.dart';
+import 'package:shopzen/core/utils/app_color.dart';
+import 'package:shopzen/core/utils/font_weight_helper.dart';
+import 'package:shopzen/feature/auth/domain/validation/validate_password_update.dart';
+import 'package:shopzen/feature/auth/domain/validation/validate_user_Iinputs.dart';
+
+class PasswordUpdateForm extends StatefulWidget {
+  const PasswordUpdateForm({super.key});
+
+  @override
+  State<PasswordUpdateForm> createState() => _PasswordUpdateFormState();
+}
+
+class _PasswordUpdateFormState extends State<PasswordUpdateForm> {
+  @override
+  void initState() {
+    super.initState();
+    ValidateUserIinputs.initController(controller: ValidatePasswordUpdate());
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    ValidateUserIinputs.disposeController(
+      disposeController: ValidatePasswordUpdate(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: ValidatePasswordUpdate.formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          AppSize.gapH48,
+          formTitel(context: context, title: context.l10n.newPassword),
+          AppSize.gapH4,
+          CustomTextField(
+            controller: ValidatePasswordUpdate.newPasswordController,
+            hintText: context.l10n.enterYourNewPassword,
+            validator: (vlaue) {
+              if (vlaue == null || vlaue.isEmpty) {
+                return context.l10n.pleaseEnterYourNewPassword;
+              } else {
+                return null;
+              }
+            },
+          ),
+          AppSize.gapH16,
+          formTitel(context: context, title: context.l10n.confirmPassword),
+          AppSize.gapH4,
+          CustomTextField(
+            controller: ValidatePasswordUpdate.confirmPasswordController,
+            hintText: context.l10n.enterYourConfirmPassword,
+            validator: (vlaue) {
+              if (vlaue == null || vlaue.isEmpty) {
+                return context.l10n.pleaseEnterYourConfirmPassword;
+              } else {
+                return null;
+              }
+            },
+          ),
+          AppSize.gapH48,
+          CustomButton(
+            child: Text(
+              context.l10n.updatePassword,
+              style: context.appTheme.textTheme.bodyMedium?.copyWith(
+                fontSize: AppSize.textSizeSp13,
+                color: context.isDark()
+                    ? AppColors.textDark
+                    : AppColors.textWhite,
+                fontWeight: FontWeightHelper.bold,
+              ),
+            ),
+            onPressed: () => {
+              ValidateUserIinputs.validateThenDoSignup(
+                validateThenDoSignup: ValidatePasswordUpdate(),
+                context: context,
+              ),
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Row formTitel({required BuildContext context, required String title}) {
+    return Row(
+      children: [
+        Text(
+          title,
+          style: context.appTheme.textTheme.bodyMedium?.copyWith(
+            fontSize: AppSize.textSizeSp12,
+            color: context.isDark() ? AppColors.textWhite : AppColors.griy1000,
+            fontWeight: FontWeightHelper.medium,
+          ),
+        ),
+      ],
+    );
+  }
+}
