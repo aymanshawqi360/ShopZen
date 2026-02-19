346d85d94974c6e0b539c73f920f9e9ef5aec582
ayman-shawqi <ayman-shawqi@ayman-shawqis-MacBook-Pro.local>
2026-02-19 22:54:00 +0300

    -added edit to splash screen

--- lib/feature/splach/presentation/screen/splach_screen.dart
+++ lib/feature/splach/presentation/screen/splach_screen.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 import 'package:shopzen/core/constants/app_assets.dart';
 import 'package:shopzen/core/constants/app_constants.dart';
 import 'package:shopzen/core/constants/app_size.dart';
 import 'package:shopzen/core/constants/app_strings.dart';
 import 'package:shopzen/core/extension/app_extension.dart';
+import 'package:shopzen/core/routing/route_name.dart';
 import 'package:shopzen/core/utils/app_color.dart';
 import 'package:shopzen/core/utils/font_weight_helper.dart';
 
@@ -21,12 +23,16 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   bool _animated = false;
   late Timer _timer;
+  late Timer _timer2;
 
   @override
   void initState() {
     super.initState();
     FlutterNativeSplash.remove();
     initialization();
+    _timer2 = Timer(Duration(seconds: 8), () {
+      context.go(AppRoutes.login);
+    });
   }
 
   void initialization() async {
@@ -55,8 +61,8 @@ class _SplashScreenState extends State<SplashScreen> {
           AnimatedPositioned(
             duration: AppConstants.animatedPositionedDuration,
             curve: Curves.easeOutBack,
-            left: _animated ? 79.w : -20.w,
-            top: MediaQuery.of(context).size.height / 2 - 25,
+            left: _animated ? 90 : -20.w,
+            top: MediaQuery.of(context).size.height / 2 - 23,
 
             child: AnimatedOpacity(
               duration: AppConstants.animatedOpacityDuration,
@@ -73,7 +79,7 @@ class _SplashScreenState extends State<SplashScreen> {
           AnimatedPositioned(
             duration: AppConstants.animatedPositionedDuration,
             curve: Curves.easeOutBack,
-            right: _animated ? 66.w : -80.w,
+            right: _animated ? 80 : -80.w,
 
             top: MediaQuery.of(context).size.height / 2.1,
             child: AnimatedOpacity(
