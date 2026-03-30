686acdb5f8e085a363297d62c17787fe427f486d
ayman-shawqi <ayman-shawqi@ayman-shawqis-MacBook-Pro.local>
2026-03-30 23:59:05 +0300

    added toke refresh function in TokenRefreshImp

---
+++ lib/core/security/implementations/token_refresh_impl.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:shopzen/core/constants/api_constants.dart';
+import 'package:shopzen/core/error/api_error_hundler.dart';
+import 'package:shopzen/core/error/api_error_model.dart';
+import 'package:shopzen/core/notworking/endpoints.dart';
+import 'package:shopzen/core/security/interfaces/i_token_refresh.dart';
+
+class TokenRefreshImpl extends ITokenRefresh {
+  TokenRefreshImpl({required super.iDecryptToken});
+  @override
+  Future<Either<Failure, void>> getRefreshToken() async {
+    try {
+      final rr = await iDecryptToken.decryptToken();
+      if (rr.isLeft()) {
+        throw Exception();
+      }
+      String data = rr.fold((_) => '', (ifRight) => ifRight);
+      final response = await Dio().post(
+        "${ApiBasUrl.baseUrl}${Endpoints.refreshToken}",
+        options: Options(headers: {"Authorization": "Bearer $data"}),
+      );
+
+      if (response.statusCode != 200 || response.data["access"] == null) {
+        throw Exception();
+      }
+      iDecryptToken.encryptionService.encrypt(
+        plaintext: response.data["access"],
+      );
+      return Right(null);
+    } catch (error) {
+      return Left(ApiErrorHundler.errorHundel(error));
+    }
+  }
+}
